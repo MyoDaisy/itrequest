19823c96cb6bac0dc29d8aad83879ca2847d387e
MyoDaisy <chicharito27101997@gmail.com>
2017-12-17 16:11:06 +0700

    2nd

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/signup', function () {
+    return view('signup');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
